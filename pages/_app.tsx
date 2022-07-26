6a65a8ba755c0ab4633cd02f962bdb4589716a71
JKTheRipperTH <69198341+JKTheRipperTH@users.noreply.github.com>
2022-07-26 13:29:21 -0700

    fixed display scrollbar
    disable scrollbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ import { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <div className="h-full gap-4 grid-cols-3 grid-rows-3 px-5 my-24 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 flex items-center justify-center ">
-        <div className="h-full col-span-4 col-start-2 p-4 text-base text-center bg-white dark:bg-dark-500 border-4 border-indigo-200 border-x-indigo-500 shadow-custom-light dark:shadow-custom-dark ">
+      <div className="h-screen gap-4 grid-cols-3 grid-rows-3 px-5 flex items-center justify-center ">
+        <div className="col-span-4 col-start-2 p-4 text-base text-center bg-white dark:bg-dark-500 border-4 border-indigo-200 border-x-indigo-500 shadow-custom-light dark:shadow-custom-dark ">
           <About />
           {/* ปิดเพราะไม่ได้ใช้ให้ไปหน้าอื่น */}
           {/* <Component {...pageProps} /> */}
