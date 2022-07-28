45687bb6cf9d8f8041d163e37a084d1630080f18
JKTheRipperTH <69198341+JKTheRipperTH@users.noreply.github.com>
2022-07-28 22:02:56 +0700

    move title in app fiex display

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import About from '../components/About'
 
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+      <title>TTT</title>
+    </Head>
     <ThemeProvider attribute="class">
-      <div className="h-screen gap-4 grid-cols-3 grid-rows-3 px-5 flex items-center justify-center ">
+      <div className="h-screen flex gap-4 grid-cols-3 grid-rows-3 px-5 items-center justify-center">
         <div className="col-span-4 col-start-2 p-4 text-base text-center bg-white dark:bg-dark-500 border-4 border-indigo-200 border-x-indigo-500 shadow-custom-light dark:shadow-custom-dark ">
           <About />
           {/* ปิดเพราะไม่ได้ใช้ให้ไปหน้าอื่น */}
@@ -15,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </div>
       </div>
     </ThemeProvider>
+    </>
   );
 }
 
